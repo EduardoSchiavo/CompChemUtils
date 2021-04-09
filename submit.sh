0dff0f620c41aab7b8f8a7563885aa59359831e9
EduardoSchiavo <81576333+EduardoSchiavo@users.noreply.github.com>
2021-04-09 11:24:58 +0200

    Update submit.sh
    Added options for zeus1

--- submit.sh
+++ submit.sh
@@ -5,7 +5,16 @@ for i in *.inp; do
 	sed -i 's/- /DA/g' $i       #substitute dummy atoms name "-" to "DA"
 done
 
+
+##xe32th1
 #Submit all *inp files with the suborca script
 for i in * inp; do
 /csnfs/software/orca/suborca -c -q xe30th ${i%.inp}  #submit all inputs
 done
+
+
+##ZEUS1
+#Submit all *inp files with the suborca script
+#for i in * inp; do
+#/opt/orca/suborca -c  ${i%.inp}  #submit all inputs
+#done
